0a84b81fe29a29c4cbe3da8eac425264675e21aa
Bill Fero <billfero@gmail.com>
2016-01-13 10:00:49 +0000

    Impl MainActivity sanity check

---
+++ app/src/test/java/com/example/downloaderdemo/MainActivityTest.java
@@ -0,0 +1,37 @@
+package com.example.downloaderdemo;
+
+import android.os.Build;
+
+import com.example.downloaderdemo.ui.MainActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivityTest {
+
+
+    private MainActivity mMActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        mMActivity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void shouldNotBeNull() throws Exception {
+        assertNotNull(mMActivity);
+    }
+
+
+}
