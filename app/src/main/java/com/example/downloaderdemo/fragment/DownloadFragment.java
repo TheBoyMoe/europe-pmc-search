9033e80177a0c2563b1abf1211e4a95cb6fec34e
Bill Fero <billfero@gmail.com>
2016-01-14 18:12:03 +0000

    Impl ViewHolder pattern with the custom adapter

--- app/src/main/java/com/example/downloaderdemo/fragment/DownloadFragment.java
+++ app/src/main/java/com/example/downloaderdemo/fragment/DownloadFragment.java
@@ -73,29 +73,28 @@ public class DownloadFragment extends BaseFragment{
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            // instantiate the list item if req'd
+
+            ViewHolder holder = null;
             if(convertView == null) {
                 convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item, null);
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            if(holder == null) {
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
             }
 
             // configure the view
             Journal journal = getItem(position);
 
-            TextView articleTitle = (TextView) convertView.findViewById(R.id.article_title);
-            TextView journalTitle = (TextView) convertView.findViewById(R.id.journal_title);
-            TextView articleAuthors = (TextView) convertView.findViewById(R.id.article_authors);
-            TextView pageInformation = (TextView) convertView.findViewById(R.id.page_information);
-            TextView journalIssue = (TextView) convertView.findViewById(R.id.journal_issue);
-            TextView journalVolume = (TextView) convertView.findViewById(R.id.journal_volume);
-            TextView publicationYear = (TextView) convertView.findViewById(R.id.publication_year);
-
-            articleTitle.setText(journal.getTitle());
-            journalTitle.setText(journal.getJournalTitle());
-            articleAuthors.setText(journal.getAuthorString());
-            pageInformation.setText(journal.getPageInfo());
-            journalIssue.setText(journal.getJournalIssn());
-            journalVolume.setText(journal.getJournalVolume());
-            publicationYear.setText(journal.getPubType());
+            holder.articleTitle.setText(journal.getTitle());
+            holder.journalTitle.setText(journal.getJournalTitle());
+            holder.articleAuthors.setText(journal.getAuthorString());
+            holder.pageInformation.setText(journal.getPageInfo());
+            holder.journalIssue.setText(journal.getJournalIssn());
+            holder.journalVolume.setText(journal.getJournalVolume());
+            holder.publicationYear.setText(journal.getPubType());
 
             return convertView;
         }
@@ -103,8 +102,28 @@ public class DownloadFragment extends BaseFragment{
 
     }
 
+    private class ViewHolder {
 
-    // TODO create custom ViewHolder
+        TextView articleTitle = null;
+        TextView journalTitle = null;
+        TextView articleAuthors = null;
+        TextView pageInformation = null;
+        TextView journalIssue = null;
+        TextView journalVolume = null;
+        TextView publicationYear = null;
+
+        ViewHolder(View view) {
+            articleTitle = (TextView) view.findViewById(R.id.article_title);
+            journalTitle = (TextView) view.findViewById(R.id.journal_title);
+            articleAuthors = (TextView) view.findViewById(R.id.article_authors);
+            pageInformation = (TextView) view.findViewById(R.id.page_information);
+            journalIssue = (TextView) view.findViewById(R.id.journal_issue);
+            journalVolume = (TextView) view.findViewById(R.id.journal_volume);
+            publicationYear = (TextView) view.findViewById(R.id.publication_year);
+        }
+
+    }
+
 
 
 }
